56eaf69f9eceadd9284d376cb0a482736a75d51f
Casper Verswijvelt <casperverswijvelt@gmail.com>
2018-11-16 14:30:45 +0100

    Cleaned up code

--- loop/loop.ino
+++ loop/loop.ino
@@ -2,206 +2,220 @@
 #include <FastLED.h>
 #define LED_PIN 5
 #define NUM_LEDS 55
-int brightness = 100;
+int brightness = 200;
 
 CRGB leds[NUM_LEDS];
 //CHSV ledsHSV[NUM_LEDS];
 
-//buttonrequirements
+//Enums
+enum ShowMode
+{
+  NONE,
+  STARTING_UP,
+  BLACKING_OUT,
+  SHOWING_BATTERY
+};
+const String showModeStrings[] = {"NONE", "STARTING_UP", "BLACKING_OUT", "SHOWING_BATTERY"};
+
+enum currentButtonState
+{
+  PRESSED = HIGH,
+  NOT_PRESSED = LOW
+};
+
+//Button information
 int buttonPin = 2;
-int buttonState; // the current reading from the input pin
-int lastButtonState = HIGH; // the previous reading from the input pin
+int currentButtonState;  // the current reading from the input pin
+int previousButtonState; // the previous reading from the input pin
 
-//button debounce
-unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
-unsigned long debounceDelay = 50; // the debounce time; increase if the output flickers
-
-//testing purposes
+//LED information
 const int ledPin = 13;
-int ledState = HIGH;
-int amountOfButtonPresses = 0;
 
-//timer requirements
 
+//Used to store currentTime
 long currentTime;
+//Used to store currentTime, after an interval
 long previousTime;
+//Stores the time when blackout was activated, so it can correctly fade over time
 long blackOutActivatedTime;
+//Stores the time that the battery was last increased, so we can fade up from this time upwards
+long batteryIncreasedTime;
 int interval = 5;
-bool timerDone = false;
-bool startUp = false;
-bool startUpDone = false;
-bool ini = false;
+
+//Mode and previous mode
+ShowMode currentMode = NONE;
+ShowMode previousMode;
+
+//To keep track at how far the animations have progressed
 int startUpSeqLedCounter = 0;
-int chargingCircleLedCounter = 0;
-int wait = 1000;
-bool waitTimer = false;
-bool waitTimerRunning = false;
-bool blackOutDone = false;
+int batteryPercentage = 0;
+
+//Configuration
 int chargingCircleSnakeLength = 55;
 int blackOutTime = 1000;
 
-
-void setup() {
+void setup()
+{
+  //Open serial monitor at 9600 to see console
   Serial.begin(9600);
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   pinMode(buttonPin, INPUT);
 
-  //blackout at setup
-  for (int j = 0; j <= NUM_LEDS; j++) {
-    leds[j] = CHSV(0, 0, 0);
-    FastLED.show();
-  }
-}
-
-void readButton() {
-  int reading = digitalRead(buttonPin);
-  currentTime = millis();
-  // Checking if the state of the button has changed since last reading
-  if (reading != lastButtonState) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
-  }
-
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    // if the button state has changed:
-    if (reading != buttonState) {
-      buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
-      if (buttonState == HIGH) {
-        ledState = !ledState;
-        amountOfButtonPresses++;
-        Serial.print("buttonState= ");
-        Serial.println(buttonState);
-        Serial.print("lastButtonState= ");
-        Serial.println(lastButtonState);
-        Serial.print("amountOfButtonPresses= ");
-        Serial.println(amountOfButtonPresses);
-      }
-    }
-  }
-  lastButtonState = reading;
-
-}
-void resetAll() {
-  timerDone = false;
-  startUp = false;
-  startUpDone = false;
-  blackOutDone = true;
-  ini = false;
-
-  waitTimer = false;
-  waitTimerRunning = false;
-  interval = 5;
-
-  startUpSeqLedCounter = 0;
-  chargingCircleLedCounter = 0;
-
-
+  //clearing all led's at setup
   clearAllLeds();
 }
-void loop() {
-  // read the state of the switch into a local variable:
-  readButton();
 
-
-  if ( buttonState == HIGH && !ini && !startUpDone) {
-    startUp = true;
-    ini = true;
-    Serial.println("first press");
-    Serial.println(ini);
-  }
-  if (buttonState == HIGH && lastButtonState == LOW ) {
-    blackOutDone = false;
-    //Call method that shows cool standby configuration
-  }
-  if (buttonState == LOW ) {
-    blackOut();
-  }
-
-
-  if (startUp) {
-    startUpSeq();
-  } else if (startUpDone) {
-    interval = 1000;
-    timerRed();
-  }
-
+void readButton()
+{
+  int reading = digitalRead(buttonPin);
+  previousButtonState = currentButtonState;
+  currentButtonState = reading;
 }
 
-void startUpSeq() {
-  if (startUpSeqLedCounter == NUM_LEDS - 1 + chargingCircleSnakeLength && startUp) {
-    startUp = false;
-    startUpDone = true;
-    Serial.println("end of startUpSeq");
+void readTime()
+{
+  currentTime = millis();
+}
+void loop()
+{
+  readTime();
+  readButton();
+
+  //If button changes from not pressed to pressed
+  if (previousButtonState == NOT_PRESSED && currentButtonState == PRESSED)
+  {
+    setCurrentMode(STARTING_UP);
+    startUpSeqLedCounter = 0;
+    clearAllLeds();
+    FastLED.setBrightness(brightness);
+  }
+  //Button changes from pressed to not pressed
+  if (previousButtonState == PRESSED && currentButtonState == NOT_PRESSED)
+  {
+    setCurrentMode(BLACKING_OUT);
+  }
+
+  //This switch case gets executed every _ miliseconds, put code here that continuously has to run according to the currentMode
+  switch (currentMode)
+  {
+  case NONE:
+    break;
+  case BLACKING_OUT:
+    blackOut();
+    break;
+  case STARTING_UP:
+    startUpSeq();
+    break;
+  case SHOWING_BATTERY:
+    interval = 1000;
+    timerRed();
+    break;
+  }
+}
+
+void startUpSeq()
+{
+  if (startUpSeqLedCounter == NUM_LEDS - 1 + chargingCircleSnakeLength)
+  {
+    setCurrentMode(SHOWING_BATTERY);
     startUpSeqLedCounter = 0;
     return;
   }
 
-  if (startUpSeqLedCounter < NUM_LEDS) {
+  if (startUpSeqLedCounter < NUM_LEDS)
+  {
     leds[startUpSeqLedCounter] = CHSV(200, 0, brightness);
   }
 
-
-  if (startUpSeqLedCounter >= chargingCircleSnakeLength) {
+  if (startUpSeqLedCounter >= chargingCircleSnakeLength)
+  {
     int turnOffLedIndex = startUpSeqLedCounter - chargingCircleSnakeLength;
     leds[turnOffLedIndex] = CHSV(0, 0, 0);
   }
 
-  if (currentTime - previousTime > 30) {
-    timerDone = true;
+  if (currentTime - previousTime > 30)
+  {
     previousTime = currentTime;
     startUpSeqLedCounter++;
   }
 
   FastLED.show();
-
 }
 
-void blackOut() {
-  //If blackoutActivated time is not yet set and a blackout is not done yet, we should fill the variable
-  if (blackOutActivatedTime == 0 && !blackOutDone) {
-    blackOutActivatedTime = currentTime;
-    Serial.println("black out activated time set");
-    Serial.println(blackOutActivatedTime);
-  }
-  
-  if (currentTime - blackOutActivatedTime < blackOutTime && !blackOutDone) {
+void blackOut()
+{
+  if (currentTime - blackOutActivatedTime < blackOutTime)
+  {
     float timeDifference = currentTime - blackOutActivatedTime;
-    FastLED.setBrightness((1 - (timeDifference / blackOutTime)) * 200);
-  }
-  
-  if (currentTime - blackOutActivatedTime > blackOutTime && !blackOutDone) {
-    blackOutActivatedTime = 0;
-    blackOutDone = true;
-    resetAll();
-    FastLED.setBrightness(200);
+    FastLED.setBrightness((1 - (timeDifference / blackOutTime)) * brightness);
   }
 
+  if (currentTime - blackOutActivatedTime > blackOutTime)
+  {
+    setCurrentMode(NONE);
+  }
 
   FastLED.show();
-
-
 }
 
-void timerRed() {
+void timerRed()
+{
 
-  if (currentTime - previousTime > interval) {
+  /*if (currentTime - previousTime > interval) {
     timerDone = true;
     previousTime = currentTime;
-    if (++chargingCircleLedCounter == NUM_LEDS - 1) {
+    if (++chargingCircleLedIndex == NUM_LEDS - 1) {
       startUp = false;
     }
+  }*/
+  if (random(0, 1000) > 990)
+  {
+    batteryPercentage++;
+    batteryIncreasedTime = currentTime;
   }
-  float timeDifference = (currentTime - previousTime);
+
+  int chargingCircleLedIndex = (float)batteryPercentage / 100 * NUM_LEDS;
+
+  float timeDifference = (currentTime - batteryIncreasedTime);
   float adjustedBrightness = timeDifference / interval * brightness;
-  leds[chargingCircleLedCounter] = CHSV(0 - map(chargingCircleLedCounter, 0, NUM_LEDS, 0, 180), 255, adjustedBrightness);
+  for (int i = 0; i < chargingCircleLedIndex; i++)
+  {
+    leds[i] = CHSV(0 - map(i, 0, NUM_LEDS, 0, 180), 255, brightness);
+  }
+  leds[chargingCircleLedIndex] = CHSV(0 - map(chargingCircleLedIndex, 0, NUM_LEDS, 0, 180), 255, adjustedBrightness);
   FastLED.show();
 }
 
-void clearAllLeds() {
-  Serial.print("All leds reset");
-  for(int t=0; t < NUM_LEDS; t++) {
-    leds[t] = CHSV(0,0,0);
+void clearAllLeds()
+{
+  for (int t = 0; t < NUM_LEDS; t++)
+  {
+    leds[t] = CHSV(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+void setCurrentMode(ShowMode mode)
+{
+  if (currentMode != mode)
+  {
+    previousMode = currentMode;
+    currentMode = mode;
+
+    //This switch case only gets executed when the mode CHANGES, to do thigs continuously according to a
+    //specific mode, do it in the switch case in the 'loop' method
+    switch (currentMode)
+    {
+    case NONE:
+      clearAllLeds();
+      break;
+    case BLACKING_OUT:
+      blackOutActivatedTime = currentTime;
+      startUpSeqLedCounter = 0;
+      break;
+    }
+
+    Serial.print("Switched to mode: ");
+    Serial.println(showModeStrings[currentMode]);
   }
 }
